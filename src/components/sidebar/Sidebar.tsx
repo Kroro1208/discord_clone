6d90a6a758ddb205e617341986516bf6a3984d86
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-20 18:57:27 +0900

    sideBarのアイコンのスタイリングした

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,10 +7,19 @@ function Sidebar() {
         <div className='sidebar'>
             {/* sidebarLeft */}
             <div className='sidebarLeft'>
-                
+                <div className='serverIcon'>
+                    <img src="./logo192.png" alt="" />
+                </div>
+                <div className='serverIcon'>
+                    <img src="./logo192.png" alt="" />
+                </div>
             </div>
             {/* sidebarRight */}
-            <div className='sidebarRight'></div>
+            <div className='sidebarRight'>
+                <div className='sidebarTop'>
+                    <h3>Discord</h3>
+                </div>
+            </div>
         </div>
     )
 }
