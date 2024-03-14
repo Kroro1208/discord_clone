b57e6792d6f0e4c0beb059883f5c796d492b6d9e
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-14 23:10:37 +0900

    user認証によるログイン機能を実装してページ遷移させた

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,38 @@ import './App.scss';
 import Chat from './components/chat/Chat';
 import Sidebar from './components/sidebar/Sidebar';
 import Login from './components/login/login';
-import { useAppSelector } from './app/hooks';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { login, logout } from './features/userSlice';
 
 function App() {
 
   const user = useAppSelector((state) => state.user);
 
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+      if (loginUser) {
+        dispatch(login({
+          userId: loginUser.uid,
+          image: loginUser.photoURL,
+          email: loginUser.email,
+          displayName: loginUser.displayName
+        }));
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="App">
       {user ? (
         <>
-          {/* sidebar */}
           <Sidebar />
-          {/* Chat */}
           <Chat />
         </>
       ) : (
--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,13 +1,22 @@
+import { signInWithPopup } from "firebase/auth";
 import "./Login.scss"
 import { Button } from '@mui/material';
+import { auth, provider } from "../../firebase";
 
 export default function Login() {
+
+    const signIn = () => {
+        signInWithPopup(auth, provider).catch((error) => {
+            alert(error.message);
+        });
+    }
+
     return (
         <div className='login'>
             <div className='loginLogo'>
                 <img src="/logo192.png" alt="" />
             </div>
-            <Button>Loginする</Button>
+            <Button onClick={signIn}>Loginする</Button>
         </div>
     );
 }
